bc19f6d9f45df21ba20d6827c6178903a8db7811
noobpk <ltp.noobpk@gmail.com>
2021-12-08 17:15:02 +0700

    update handlers.js script Now we can intercept request and response API

--- handlers.js
+++ handlers.js
@@ -1,7 +1,4 @@
-
-var search_class = [''];
-var search_method = [''];
-
+/*Terminal Color*/
 var colors = {
     "resetColor": "\x1b[0m",
     "green": "\x1b[32m",
@@ -9,15 +6,24 @@ var colors = {
     "red": "\x1b[31m"
 }
 
-function search_methods(className) {
-    var methods_found = [];
+/*Request Class & Method*/
+var search_request_class  = [''];
+var search_request_method = [''];
+
+/*Response Class & Method*/
+var search_response_class  = [''];
+var search_response_method = [''];
+
+/*Function Search Request Method*/
+function search_request_methods(className) {
+    var request_methods_found = [];
     var methods = ObjC.classes[className].$ownMethods;
-    if (Array.isArray(search_method) && search_method.length) { //search_method not empty
-        for (var j = 0; j < search_method.length; j++) {
-            if (methods.join(' ').toLowerCase().includes(search_method[j].toLowerCase())) {
+    if (Array.isArray(search_request_method) && search_request_method.length) { //search_request_method not empty
+        for (var j = 0; j < search_request_method.length; j++) {
+            if (methods.join(' ').toLowerCase().includes(search_request_method[j].toLowerCase())) {
                 for (var i = 0; i < methods.length; i++){
-                    if (methods[i].toLowerCase().includes(search_method[j].toLowerCase())) {
-                        methods_found.push(methods[i]);
+                    if (methods[i].toLowerCase().includes(search_request_method[j].toLowerCase())) {
+                        request_methods_found.push(methods[i]);
                     }
                 }
             }
@@ -26,24 +32,64 @@ function search_methods(className) {
     else {
         var methods = ObjC.classes[className].$ownMethods;
         for (var i = 0; i < methods.length; i++){
-            methods_found.push(methods[i]);
+            request_methods_found.push(methods[i]);
         }
     }
-    return methods_found;
+    return request_methods_found;
 }
 
-function search_classes(){
-    var classes_found = [];
+/*Function Search Request Class*/
+function search_request_classes(){
+    var classes_request_found = [];
     for (var className in ObjC.classes) {
-        if (Array.isArray(search_class) && search_class.length) {
-            for (var i = 0; i < search_class.length; i++) {
-                if (className.toLowerCase().includes(search_class[i].toLowerCase())) {
-                    classes_found.push(className);
+        if (Array.isArray(search_request_class) && search_request_class.length) {
+            for (var i = 0; i < search_request_class.length; i++) {
+                if (className.toLowerCase().includes(search_request_class[i].toLowerCase())) {
+                    classes_request_found.push(className);
                 }
             }
         }
     }
-    return classes_found;
+    return classes_request_found;
+}
+
+/*Function Search Response Method*/
+function search_response_methods(className) {
+    var response_methods_found = [];
+    var methods = ObjC.classes[className].$ownMethods;
+    if (Array.isArray(search_response_method) && search_response_method.length) { //search_response_method not empty
+        for (var j = 0; j < search_response_method.length; j++) {
+            if (methods.join(' ').toLowerCase().includes(search_response_method[j].toLowerCase())) {
+                for (var i = 0; i < methods.length; i++){
+                    if (methods[i].toLowerCase().includes(search_response_method[j].toLowerCase())) {
+                        response_methods_found.push(methods[i]);
+                    }
+                }
+            }
+        }
+    }
+    else {
+        var methods = ObjC.classes[className].$ownMethods;
+        for (var i = 0; i < methods.length; i++){
+            response_methods_found.push(methods[i]);
+        }
+    }
+    return response_methods_found;
+}
+
+/*Function Search Response Class*/
+function search_response_classes(){
+    var classes_response_found = [];
+    for (var className in ObjC.classes) {
+        if (Array.isArray(search_response_class) && search_response_class.length) {
+            for (var i = 0; i < search_response_class.length; i++) {
+                if (className.toLowerCase().includes(search_response_class[i].toLowerCase())) {
+                    classes_response_found.push(className);
+                }
+            }
+        }
+    }
+    return classes_response_found;
 }
 
 function print_arguments(args) {
@@ -71,27 +117,29 @@ It stops when:
 if (ObjC.available)
 {
     console.log(colors.green,"\n[*] Started: Hooking.... ",colors.resetColor);
-    var classes_found = search_classes();
-    for (var i = 0; i < classes_found.length; ++i) {
-        var methods_found = 0;
-        methods_found = search_methods(classes_found[i]);
+    console.log(colors.green,"\n[*] Hooking Request: ",colors.resetColor);
+    var classes_request_found = search_request_classes();
+    for (var i = 0; i < classes_request_found.length; ++i) {
+        var methods_request_found = 0;
+        methods_request_found = search_request_methods(classes_request_found[i]);
 
-        if (Object.keys(methods_found).length){
-            console.log(classes_found[i]);
+        if (Object.keys(methods_request_found).length){
+            console.log(classes_request_found[i]);
         }
-        for (var j = 0; j < methods_found.length; ++j) {
-            var _className = "" + classes_found[i];
-            var _methodName = "" + methods_found[j];
+        for (var j = 0; j < methods_request_found.length; ++j) {
+            var _className = "" + classes_request_found[i];
+            var _methodName = "" + methods_request_found[j];
             var hooking = ObjC.classes[_className][_methodName];
-            console.log('   ' + methods_found[j]);
+            console.log('   ' + methods_request_found[j]);
 
             Interceptor.attach(hooking.implementation, {
                 onEnter: function (args, state) {
                     this._className = ObjC.Object(args[0]).toString();
                     this._methodName = ObjC.selectorAsString(args[1]);
-                    console.log(colors.green,"[+] Detected call to: ",colors.resetColor);
+                    console.log(colors.green,"[REQUEST] Detected call to: ",colors.resetColor);
                     console.log('   ' + this._className + ' --> ' + this._methodName);
-                    console.log(colors.green,"[+] Dump Arugment in method: ",colors.resetColor);
+                    console.log(colors.green,"[REQUEST] Dump Arugment in method: ",colors.resetColor);
+                    /*DEBUG ARGS*/
                     //print_arguments(args);
                     // console.log(ObjC.Object(args[3]));
                     // var message1 = ObjC.Object(args[2]);
@@ -140,9 +188,80 @@ if (ObjC.available)
                 }
             });
         }
-
     }
-    console.log('\n[*] Starting Intercepting');
+    console.log(colors.green,"\n[*] Hooking Response: ",colors.resetColor);
+    var classes_response_found = search_response_classes();
+    for (var i = 0; i < classes_response_found.length; ++i) {
+        var methods_response_found = 0;
+        methods_response_found = search_response_methods(classes_response_found[i]);
+
+        if (Object.keys(methods_response_found).length){
+            console.log(classes_response_found[i]);
+        }
+        for (var j = 0; j < methods_response_found.length; ++j) {
+            var _className = "" + classes_response_found[i];
+            var _methodName = "" + methods_response_found[j];
+            var hooking = ObjC.classes[_className][_methodName];
+            console.log('   ' + methods_response_found[j]);
+
+            Interceptor.attach(hooking.implementation, {
+                onEnter: function (args, state) {
+                    this._className = ObjC.Object(args[0]).toString();
+                    this._methodName = ObjC.selectorAsString(args[1]);
+                    console.log(colors.green,"[RESPONSE] Detected call to: ",colors.resetColor);
+                    console.log('   ' + this._className + ' --> ' + this._methodName);
+                    console.log(colors.green,"[RESPONSE] Dump Arugment in method: ",colors.resetColor);
+                    /*DEBUG ARGS*/
+                    //print_arguments(args);
+                    //console.log(ObjC.Object(args[2]));
+                    // var message1 = ObjC.Object(args[2]);
+                    // var message2 = ObjC.Object(args[3]);
+                    // var message3 = ObjC.Object(args[4]);
+
+                    // console.log('msg1=' + message1.toString() + ",type: "+ message1.$className);
+                    // console.log('msg2=' + message2.toString() + ",type: "+ message2.$className);
+                    // console.log('msg3=' + message3.toString() + ",type: "+ message3.$className);
+                    
+
+                    this.buf = ObjC.Object(args[2]).toString();
+
+                    var js = {};
+                    var dict = new ObjC.Object(args[2]);
+                    var enumerator = dict.keyEnumerator();
+                    var key;
+                    while((key = enumerator.nextObject()) !== null){
+                        var value = dict.objectForKey_(key);
+                        js[key] = value.toString();
+                    }
+
+                    console.log('js:', JSON.stringify(js));
+
+                    send({from: '/http', payload: JSON.stringify(js)})
+                    var op = recv('input', function(value) { // callback function
+                        console.log("Forwarding mitm'ed content: " + value.payload);
+                        var js = JSON.parse(value.payload);
+                        console.log('js response:', js);
+                        var param_dict = ObjC.classes.NSMutableDictionary.alloc().init();
+                        var NSString = ObjC.classes.NSString;    
+                        for(var key in js){
+                            if(js.hasOwnProperty(key)){
+                                console.log(key + " -> " + js[key]);
+                                var valueObject = NSString.stringWithString_(js[key]); 
+                                param_dict.setObject_forKey_(valueObject, key);
+                            }
+                        }
+                        console.log('param_dict:', param_dict);
+                        args[2] = param_dict;
+                    });
+                    op.wait();
+                },
+                onLeave: function(retval, state) {
+                    
+                }
+            });
+        }
+    }
+    console.log(colors.green,"\n[*] Starting Intercepting\n",colors.resetColor);
 }
 else {
     console.log('Objective-C Runtime is not available!');
